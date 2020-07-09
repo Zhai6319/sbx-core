929d4caacd2e439a9888327662f8c24c177d5c14
zhaijianchao <zhai_spir@sbxiang.com>
2020-07-09 18:38:36 +0800

    核心代码提交

--- sbx-core-cloud/src/main/java/org/sbx/core/cloud/annotation/EnableSbxFeignClients.java
+++ sbx-core-cloud/src/main/java/org/sbx/core/cloud/annotation/EnableSbxFeignClients.java
@@ -4,10 +4,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 import java.lang.annotation.*;
 
-/**
- * 开启十店openfeign客户端注解
- * @author zhaijianchao
- */
+
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE})
 @Documented
